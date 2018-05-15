5e4231704edac215a247341da60d26b0c75e7b70
xingyun86 <xingyun_ppshuai@yeah.net>
2018-05-15 19:31:56 +0800

    提交zlib的vs工程

---
+++ src/vcprojects/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/DevelopmentEnvironment/Workspaces/Sources/zlib-1.2.11-src/zlib-1.2.11
+# Build directory: D:/DevelopmentEnvironment/Workspaces/Sources/zlib-1.2.11-src/vcprojects
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
